5da024abf8ce0f4130cc419e849552d12c02ee85
Igor Russu <maildevcom@gmail.com>
2016-04-12 08:11:58 +0300

    - Typical client.js has been added;

---
+++ client.js
@@ -0,0 +1,28 @@
+if (window.WebSocket) {
+   console.log("WebSocket is supported by your Browser!");
+
+   // Let us open a web socket
+   var ws = new WebSocket("ws://localhost:8080");
+
+   ws.onopen = function()
+   {
+      // Web Socket is connected, send data using send()
+      ws.send("Message to send");
+      console.log("Message is sent...");
+   };
+
+   ws.onmessage = function (evt)
+   {
+      var received_msg = evt.data;
+      console.log("Message is received..." + received_msg);
+   };
+
+   ws.onclose = function()
+   {
+      // websocket is closed.
+      console.log("Connection is closed...");
+   };
+} else {
+   // The browser doesn't support WebSocket
+   console.log("WebSocket NOT supported by your Browser!");
+}
