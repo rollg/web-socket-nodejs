b19db3e8a345a7b07b0e49a243bb5afce2b1a59e
Igor Russu <maildevcom@gmail.com>
2016-04-14 13:58:56 +0300

    <button> has been created to start socket functionality

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 var WebSocketServer = require('ws').Server
 var wss = new WebSocketServer({ port: 8080 });
 
-console.log(wss.options.port);
+var express = require('express');
+var app = express();
+var serverUrl = "localhost";
+
+app.post('/', function (req, res) {
+  console.log('connected');
+  res.end();
+});
+
+app.use(express.static(__dirname + '/'));
+
+var listener = app.listen(8000, function(){
+    console.log("Client Server is listening at " + serverUrl + ":" + listener.address().port);
+});
+console.log("Web Socket Server is running at " + serverUrl + ":" + wss.options.port);
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
